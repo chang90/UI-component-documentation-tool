b15bdcc9210adfa6dae4863eebad2eeabe96621e
hiby90hou <chang.liu.programmer@gmail.com>
2021-02-20 16:30:22 +1100

    able to load react component in iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () =>{
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -28,9 +29,15 @@ const App = () =>{
           global: 'window'
         }
       });
+    // setCode(result.outputFiles[0].text);
+      // try{
+      //   eval(result.outputFiles[0].text);
+      // } catch (err) {
+      //   alert(err);
+      // }
 
-    console.log(result)
-    setCode(result.outputFiles[0].text);
+      iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    
   }
 
   const startService = async () => {
@@ -44,6 +51,20 @@ const App = () =>{
     startService();
   },[])
 
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) =>{
+            eval(event.data)
+          })
+        </script>
+      </body>
+    </html>
+`;
+
   return (
   <div>
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
@@ -51,8 +72,11 @@ const App = () =>{
       <button onClick={onClick}>Submit</button>
     </div>
     <pre>{code}</pre>
+    <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html}></iframe>
   </div>);
 }
+
+
 
 ReactDOM.render(
   <App />,
