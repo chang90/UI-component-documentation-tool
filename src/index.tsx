0665c848f7e9abf2b759d8636d5b56823848bc7f
hiby90hou <chang.liu.programmer@gmail.com>
2021-02-20 16:40:09 +1100

    add error handling

--- src/index.tsx
+++ src/index.tsx
@@ -58,8 +58,13 @@ const App = () =>{
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) =>{
-            eval(event.data)
-          })
+            try{
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;">' + err + '</div>'
+            }
+          }, false)
         </script>
       </body>
     </html>
