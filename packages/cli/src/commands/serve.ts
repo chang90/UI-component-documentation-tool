1765f8563f7ae201a3b216ca8d38c501f413aec7
hiby90hou <chang.liu.programmer@gmail.com>
2021-03-13 17:09:43 +1100

    create express service

---
+++ packages/cli/src/commands/serve.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import { Command } from 'commander';
+import { serve } from 'local-api';
+
+export const serveCommand = new Command()
+  .command('serve [filename]')
+  .description('Open a file for editing')
+  .option('-p, --port <number>', 'port to run sever on', '4005')
+  .action(async (filename = 'notebook.js', options: {port: string}) => {
+    try{
+      const dir = path.join(process.cwd(), path.dirname(filename))
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`);
+    } catch (err) {
+      if(err.code === 'EADDRINUSE') {
+        console.log('Port is being used, please use another one')
+      } else {
+        console.log('Error:', err.message);
+      }
+      process.exit(1);
+    }
+  })
