e2e7a528e1ca094cd15c91ac06905f4fa7d23b89
hiby90hou <chang.liu.programmer@gmail.com>
2021-03-13 23:00:07 +1100

    try to modify package.json

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -22,7 +22,7 @@ export const serve = (
     }))
   } else {
     // Running in a user's machine
-    const packagePath = require.resolve('local-client/build/index.html');
+    const packagePath = require.resolve('@hiby90/local-client/build/index.html');
     app.use(express.static(path.dirname(packagePath)));
   }
 
